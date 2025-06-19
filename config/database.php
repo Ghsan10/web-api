42ac2921e482254c90f06fc39754d5abdf4652b8
241 Panggih Santri <panggihsantrisi4g@gmail.com>
2025-06-19 23:27:39 +0700

    commit kedua setelah setting railway

--- config/database.php
+++ config/database.php
@@ -1,8 +1,14 @@
 <?php
-$host = 'localhost';
-$dbname = 'gudang_db';
+// $host = 'localhost';
+// $dbname = 'gudang_db';
+// $username = 'root';
+// $password = '';
+
+$host = 'mysql.railway.internal';
+$dbname = 'railway';
 $username = 'root';
-$password = '';
+$password = 'YqgbKBTiDvxzMYjzAFxOnZFvNJoYQVet';
+$port = 3306;
 
 try {
     $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
