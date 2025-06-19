29b2544fccaa238e16f4cb46b3db1d45d006f1b5
241 Panggih Santri <panggihsantrisi4g@gmail.com>
2025-06-19 23:51:08 +0700

    database.php new

--- config/database.php
+++ config/database.php
@@ -3,15 +3,20 @@
 // $dbname = 'gudang_db';
 // $username = 'root';
 // $password = '';
-$host = 'nozomi.proxy.rlwy.net';
-$dbname = 'railway';
-$username = 'root';
-$password = 'YqgbKBTiDvxzMYjzAFxOnZFvNJoYQVet';
+$host = "nozomi.proxy.rlwy.net";
+$dbname = "railway";
+$username = "root";
+$password = "YqgbKBTiDvxzMYjzAFxOnZFvNJoYQVet";
 $port = 32871;
 
 try {
-    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $db = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     die("Koneksi gagal: " . $e->getMessage());
 }
